ba87be0ef681ef7df92630934e58c60800b12b99
Jonathan Lin <jlin1999@gmail.com>
2020-01-22 21:43:12 -0500

    functional_diagram_add

--- code/wifi_credentials.hpp
+++ code/wifi_credentials.hpp
@@ -1,4 +1,4 @@
 // User wi-fi credentials
 
-extern const char network_name[64] = "ATTW6YS4I2";
-extern const char network_password[64] = "c%qi=ijne=t+";
+extern const char network_name[64] = "Camelot";
+extern const char network_password[64] = "Ashcity123456";
